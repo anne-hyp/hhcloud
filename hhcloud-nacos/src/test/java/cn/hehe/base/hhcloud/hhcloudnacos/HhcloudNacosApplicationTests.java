ec3425007224f22dabdb39eaf81e03bd19643797
黄奕鹏 <1074136068@qq.com>
2022-03-10 20:34:12 +0800

    提交初始代码

---
+++ hhcloud-nacos/src/test/java/cn/hehe/base/hhcloud/hhcloudnacos/HhcloudNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hehe.base.hhcloud.hhcloudnacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhcloudNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/cn/hehe/base/hhcloud/HhcloudApplication.java
@@ -0,0 +1,13 @@
+package cn.hehe.base.hhcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhcloudApplication.class, args);
+    }
+
+}
